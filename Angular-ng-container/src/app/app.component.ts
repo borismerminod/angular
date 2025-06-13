4f059ab63990885198fa9331d3ff0a886caba0dd
bmermino <boris.merminod@edu.univ-fcomte.fr>
2025-06-13 23:35:37 +0200

    Using ng-container

---
+++ Angular-ng-container/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Angular-ng-container';
+
+  toggle: Boolean = false
+  onToggle()
+  {
+    this.toggle = !this.toggle
+  }
+}
